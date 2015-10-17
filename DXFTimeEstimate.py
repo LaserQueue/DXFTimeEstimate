625ad6bd3ece2b08a9deb55d1f4b1bb96901f56f
sdaitzman <samuel@daitzman.com>
2015-10-17 15:06:05 -0400

    allows DXF files to be uploaded

---
+++ DXFTimeEstimate.py
@@ -0,0 +1,13 @@
+from QueueConfig import *
+from jsonhandler import SocketCommand
+
+printer = PluginPrinterInstance()
+printer.setname("DXFTimeEst")
+
+def receive_dxf(**kwargs):
+	printer.color_print("DXF file received.")
+	# print(kwargs["args"]["dxf_data"])
+
+socketCommands = [
+	SocketCommand("send_dxf", receive_dxf, {"dxf_data": str})
+]
